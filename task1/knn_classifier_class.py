d4aa1e0baff548bcbae8d03d3a35dea2935e85ce
Neo <petrenko-m-p@mail.ru>
2021-11-09 13:47:26 +0700

    task1

--- task1/knn_classifier_class.py
+++ task1/knn_classifier_class.py
@@ -12,13 +12,26 @@ class KnnPowerClassifier(BaseEstimator, ClassifierMixin):
         self.k = k
         self.iterations = iterations
         
-        self.reduced_weights = np.array([])
-        self.reduced_x = np.array([])
-        self.reduced_y = np.array([])
+        self.reduced_weights = self.current_weights = np.array([])
+        self.reduced_x = self.current_x = np.array([])
+        self.reduced_y = self.current_y = np.array([])
 
         self.max_distance = 0
+        self.trained = False
 
+    
+    def reduce(self):
+        if not self.trained:
+            raise Exception('model is not trained')
+
+        self.current_x = self.reduced_x
+        self.current_y = self.reduced_y
+        self.current_weights = self.reduced_weights
+
+    
     def fit(self, X, y):
+        self.trained = True
+
         self.max_distance =  max([max([self.metric(y, x) for x in X]) for y in X]) * 2 #max(self.metric(X[:, np.newaxis, :] - X[np.newaxis, :, :])) 
         weights = remove_redundant_points(X, y, self.k, self.metric, self.kernel, self.max_distance, self.iterations)
         non_zero_ids = [i for i in range(weights.shape[0]) if weights[i] > 0]
@@ -27,5 +40,11 @@ class KnnPowerClassifier(BaseEstimator, ClassifierMixin):
         self.reduced_y = y[non_zero_ids]
         self.reduced_weights = weights[non_zero_ids]
         
+        #for now use full data
+        self.current_x = X
+        self.current_y = y
+        self.current_weights = weights
+
+
     def predict(self, X):
-        return np.asarray([knn(_x, self.reduced_x, self.reduced_y, self.k, self.metric, self.kernel, self.max_distance, self.reduced_weights) for _x in X])
+        return np.asarray([knn(_x, self.current_x, self.current_y, self.k, self.metric, self.kernel, self.max_distance, self.current_weights) for _x in X])
